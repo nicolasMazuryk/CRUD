f42a222388b45c0708fecc952b540ef8f8a41be0
nicolasMazuryk <life8063722@gmail.com>
2015-12-19 01:56:32 +0200

    expressjs initial

--- app/server.js
+++ app/server.js
@@ -1,4 +1,5 @@
 var http = require('http'),
+    express = require('express'),
     fs = require('fs'),
     qs = require('querystring'),
     database = require( './database.json'),
@@ -7,137 +8,159 @@ var http = require('http'),
     port = process.env.PORT || 4000,
     host = process.env.HOST || '0.0.0.0',
     storage,
-    body = '',
-    server;
+    //body = '',
+    app = express();
 
-var DatabaseInit = function () {
+app.get('/', function (err, req, res, next ) {
 
-        this.storageLength = database.storageLength ?
-            database.storageLength : 0;
+    fs.readFile( path.join(__dirname, 'index.html') , function ( err, data ) {
+        if ( err ) {
+            res.statusCode = 404;
+            res.end('The browser cannot load the page from server. Sorry for that');
+        }
 
-        this.saves = database.saves ?
-            database.saves : [];
-    };
-
-storage = new DatabaseInit();
-
-
-server = new http.Server(function( req, res ) {
-    //console.log( req.headers.origin);
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST', 'PUT', 'DELETE');
-
-    if (req.url == '/') {
-
-        fs.readFile( path.join(__dirname, 'index.html') , function ( err, data ) {
-            if ( err ) {
-                res.statusCode = 404;
-                res.end('The browser cannot load the page from server. Sorry for that');
-            }
-
-            res.writeHead(200, {"Content-Type": mime.lookup(path.basename((path.join(__dirname, 'index.html'))))});
-            res.end( data );
-        });
-
-    } else if (req.url == '/saves') {
-
-        res.statusCode = 200;
-        res.end( JSON.stringify( storage ) );
-
-    } else if ( req.url == '/edit') {
-
-        var body = '';
-
-        req.on( 'data', function ( data ) {
-
-            body += data;
-
-        });
-
-        req.on('end', function() {
-
-            var edited = JSON.parse( body );
-
-            for (var i = 0, l = storage.storageLength; i < l; i += 1 ) {
-                if (storage.saves[i]._id === edited._id ) {
-                    storage.saves[ i ] = edited;
-                    break;
-                }
-            }
-            fs.writeFile( 'database.json', JSON.stringify( storage ) );
-
-            res.statusCode = 200;
-            res.end( JSON.stringify( storage ) );
-        });
-
-    } else if ( req.url == '/remove' ) {
-
-        var body = '';
-
-        req.on( 'data', function ( data ) {
-            body += data;
-
-        });
-
-        req.on('end', function() {
-
-            var removed = JSON.parse( body );
-
-            for (var i = 0, l = storage.storageLength; i < l; i += 1 ) {
-                if (storage.saves[i]._id === removed._id ) {
-                    storage.saves.splice( i, 1 );
-                    storage.storageLength -= 1;
-                    break;
-                }
-            }
-            fs.writeFile( 'database.json', JSON.stringify( storage ) );
-
-            res.statusCode = 200;
-            res.end( JSON.stringify( storage ) );
-        });
-
-    } else if ( req.url == '/post' ) {
-
-        body = '';
-
-        req.on('data', function (data) {
-
-            body += data;
-
-        });
-
-        req.on('end', function() {
-
-            var data = JSON.parse( body );
-
-            storage.saves[storage.storageLength] = data;
-
-
-            console.log( "Parsed data: ", data );
-            console.log( "Storage.saves: ", storage.saves );
-
-            storage.storageLength += 1;
-
-            fs.writeFile( 'database.json', JSON.stringify( storage ) );
-
-            res.statusCode = 200;
-            res.end( JSON.stringify( storage ) );
-        });
-    } else {
-        fs.readFile( path.join(__dirname, './' + req.url) , function ( err, data ) {
-            if ( err ) {
-                res.statusCode = 404;
-                res.end('The browser cannot load the page from server. Sorry for that');
-            } else {
-                res.writeHead(200, {"Content-Type": mime.lookup(path.basename((path.join(__dirname, './' + req.url))))});
-                res.end( data );
-            }
-        });
-    }
+        res.writeHead(200, {"Content-Type": mime.lookup(path.basename((path.join(__dirname, 'index.html'))))});
+        res.end( data );
+    });
 
 });
 
-server.listen(port, host, function() {
-    console.log('Server running at ' + host + ':' + port);
+
+var server = app.listen(port, function () {
+    var host = server.address().address,
+        port = server.address().port;
+
+    console.log('Server listening at http://' + host + ':' + port);
 });
+
+//var DatabaseInit = function () {
+//
+//        this.storageLength = database.storageLength ?
+//            database.storageLength : 0;
+//
+//        this.saves = database.saves ?
+//            database.saves : [];
+//    };
+//
+//storage = new DatabaseInit();
+//
+//
+//server = new http.Server(function( req, res ) {
+//    //console.log( req.headers.origin);
+//    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//    res.setHeader('Access-Control-Allow-Origin', '*');
+//    res.setHeader('Access-Control-Allow-Methods', 'GET, POST', 'PUT', 'DELETE');
+//
+//    if (req.url == '/') {
+//
+//        fs.readFile( path.join(__dirname, 'index.html') , function ( err, data ) {
+//            if ( err ) {
+//                res.statusCode = 404;
+//                res.end('The browser cannot load the page from server. Sorry for that');
+//            }
+//
+//            res.writeHead(200, {"Content-Type": mime.lookup(path.basename((path.join(__dirname, 'index.html'))))});
+//            res.end( data );
+//        });
+//
+//    } else if (req.url == '/saves') {
+//
+//        res.statusCode = 200;
+//        res.end( JSON.stringify( storage ) );
+//
+//    } else if ( req.url == '/edit') {
+//
+//        var body = '';
+//
+//        req.on( 'data', function ( data ) {
+//
+//            body += data;
+//
+//        });
+//
+//        req.on('end', function() {
+//
+//            var edited = JSON.parse( body );
+//
+//            for (var i = 0, l = storage.storageLength; i < l; i += 1 ) {
+//                if (storage.saves[i]._id === edited._id ) {
+//                    storage.saves[ i ] = edited;
+//                    break;
+//                }
+//            }
+//            fs.writeFile( 'database.json', JSON.stringify( storage ) );
+//
+//            res.statusCode = 200;
+//            res.end( JSON.stringify( storage ) );
+//        });
+//
+//    } else if ( req.url == '/remove' ) {
+//
+//        var body = '';
+//
+//        req.on( 'data', function ( data ) {
+//            body += data;
+//
+//        });
+//
+//        req.on('end', function() {
+//
+//            var removed = JSON.parse( body );
+//
+//            for (var i = 0, l = storage.storageLength; i < l; i += 1 ) {
+//                if (storage.saves[i]._id === removed._id ) {
+//                    storage.saves.splice( i, 1 );
+//                    storage.storageLength -= 1;
+//                    break;
+//                }
+//            }
+//            fs.writeFile( 'database.json', JSON.stringify( storage ) );
+//
+//            res.statusCode = 200;
+//            res.end( JSON.stringify( storage ) );
+//        });
+//
+//    } else if ( req.url == '/post' ) {
+//
+//        body = '';
+//
+//        req.on('data', function (data) {
+//
+//            body += data;
+//
+//        });
+//
+//        req.on('end', function() {
+//
+//            var data = JSON.parse( body );
+//
+//            storage.saves[storage.storageLength] = data;
+//
+//
+//            console.log( "Parsed data: ", data );
+//            console.log( "Storage.saves: ", storage.saves );
+//
+//            storage.storageLength += 1;
+//
+//            fs.writeFile( 'database.json', JSON.stringify( storage ) );
+//
+//            res.statusCode = 200;
+//            res.end( JSON.stringify( storage ) );
+//        });
+//    } else {
+//        fs.readFile( path.join(__dirname, './' + req.url) , function ( err, data ) {
+//            if ( err ) {
+//                res.statusCode = 404;
+//                res.end('The browser cannot load the page from server. Sorry for that');
+//            } else {
+//                res.writeHead(200, {"Content-Type": mime.lookup(path.basename((path.join(__dirname, './' + req.url))))});
+//                res.end( data );
+//            }
+//        });
+//    }
+//
+//});
+//
+//server.listen(port, host, function() {
+//    console.log('Server running at ' + host + ':' + port);
+//});
